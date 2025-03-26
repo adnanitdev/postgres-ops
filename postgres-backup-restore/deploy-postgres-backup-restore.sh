f68049995f1aaab73b653c0a0900d979ae9b7207
Adnan Mohammad <115599232+adnanitdev@users.noreply.github.com>
2025-03-26 12:37:55 -0400

    fix namespace

--- postgres-backup-restore/deploy-postgres-backup-restore.sh
+++ postgres-backup-restore/deploy-postgres-backup-restore.sh
@@ -5,7 +5,6 @@ set -e
 CHART_NAME="postgres-backup-restore"
 RELEASE_NAME="pg-job"
 NAMESPACE="default"
-RESTORE_NAMESPACE=$(yq '.restore.namespace' values.yaml)
 
 echo "[1] Deploying Helm chart..."
 helm upgrade --install $RELEASE_NAME . --namespace $NAMESPACE --create-namespace
@@ -17,9 +16,9 @@ echo "[3] Copying dump from pg-dump pod..."
 kubectl cp $NAMESPACE/pg-dump:/dump ./pg_dump
 
 echo "[4] Copying dump to pg-restore pod..."
-kubectl cp ./pg_dump pg-restore:/dump -n $RESTORE_NAMESPACE
+kubectl cp ./pg_dump pg-restore:/dump -n $NAMESPACE
 
 echo "[5] Restoring database to target Postgres..."
-kubectl exec -it pg-restore -n $RESTORE_NAMESPACE --   psql "$(yq '.restore.uri' values.yaml)" < /dump/db.sql
+kubectl exec -it pg-restore -n $NAMESPACE --   psql "$(yq '.restore.uri' values.yaml)" < /dump/db.sql
 
 echo "✅ PostgreSQL backup and restore completed."
